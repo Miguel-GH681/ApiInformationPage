5970acd58e18e79c0354283ea54b44c3e73e0c93
Miguel-GH681 <miguelgh681@gmail.com>
2022-07-13 10:50:34 -0500

    Se agrego una ruta vacía

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
-var waterHeaterRoutes = require('./routes/waterHeater.route');
+var waterHeaterRoutes = require('./routes/waterheater.route');
 var app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
--- routes/waterheater.route.js
+++ routes/waterheater.route.js
@@ -1,10 +1,12 @@
 'use strict'
 
 var express = require('express');
+const app = require('../app');
 var waterHeaterController = require('../controllers/waterheater.controller');
 const upload = require('../utils/multer.config');
 
 var api = express.Router();
+api.get('/', (req, res)=> res.send("Hello Word"));
 api.post('/SaveWaterHeater', upload.single('image'), waterHeaterController.saveWaterHeater);
 api.get('/getWaterHeaters', waterHeaterController.getWaterHeaters);
 api.get('/getWaterHeater/:id', waterHeaterController.getWaterHeater);
